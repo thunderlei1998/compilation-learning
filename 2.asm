e04186606af3232671257505e1fb8ceba3fec5fe
thunderlei1998 <89784455+thunderlei1998@users.noreply.github.com>
2021-09-01 23:15:26 +0800

    Add files via upload

--- 2.asm
+++ 2.asm
@@ -1,20 +1,23 @@
-assume cs:codesegment
-codesegment segment
+assume cs:code,ds:data,ss:stack
 
-start:	mov ax,0ffffh
-		mov ds,ax
-		mov bx,0
-		mov dx,0
-		mov cx,12
-		
-s:		mov al,[bx]
-		mov ah,0
-		add dx,ax
-		inc bx
-		loop s
-		
-		mov ax,4c00h
-		int 21h
-		
-codesegment ends
+data segment
+dw 0123h,0456h,0789h,0abch,0defh,0fedh,0cbah,0987h
+data ends
+stack segment
+dw 0,0,0,0,0,0,0,0
+stack ends
+code segment
+start:mov ax,stack
+	mov ss,ax
+	mov sp,16
+	mov ax,data
+	mov ds,ax
+	push ds:[0]
+	push ds:[2]
+	pop ds:[2]
+	pop ds:[0]
+
+	mov ax,4c00h
+	int 21h
+code ends
 end start
